60543704c1d7608c084a46e2ed7307ce86c32112
TakashiHK123 <taka.honge@gmail.com>
2023-12-21 11:01:34 -0300

    desarrollo completo

--- ClasificadorMosquito.py
+++ ClasificadorMosquito.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # libraries
-import time
 import RPi.GPIO as GPIO
+import numpy as np
+import scipy.io.wavfile as wav
+import alsaaudio
+import time
+import pyudev
+import matplotlib.pyplot as plt
+
+# Inicializar el gráfico
+plt.ion()  # Habilitar modo interactivo
+fig, ax = plt.subplots()
+line, = ax.plot([], [])
+ax.set_title('FFT en tiempo real')
+ax.set_xlabel('Frecuencia (Hz)')
+ax.set_ylabel('Amplitud')
+#frecuencias = np.fft.fftfreq(512, 1 / 44100)  # Ajustar según el tamaño del periodo
+ax.set_xlim(0,  6000)  # Ajustar según la frecuencia de muestreo
+ax.set_ylim(-1800000, 1800000)
+longitud_senal = 1024 #Tamanho del bufer de lectura
+frecuencia_muestreo = 44100 # Establecer la frecuencia de muestreo a 42.667 kHz
 
 # Use BCM GPIO references
 # Instead of physical pin numbers
@@ -19,8 +37,19 @@ servoPIN = 13
 # GPIO.setup(succionFan, GPIO.OUT)
 # GPIO.setup(empujeFan, GPIO.OUT)
 GPIO.setup(servoPIN, GPIO.OUT)
-p = GPIO.PWM(servoPIN, 50)  # GPIO 13 for PWM with 50Hz
+servoCompuertaPIN = 6
+GPIO.setup(servoCompuertaPIN, GPIO.OUT)
+p = GPIO.PWM(servoPIN, 50)  #SERVO DEL SELECTOR FLUJO
 p.start(2.5)
+# --------------------------------------------------
+c = GPIO.PWM(servoCompuertaPIN, 50) #servo del compuerta mosquito
+c.start(2.5)
+# --------------------------------------------------
+# Define el número del pin GPIO que deseas usar
+pin_sensor = 5
+# Configura el pin como entrada
+GPIO.setup(pin_sensor, GPIO.IN)
+
 for pin in StepPins:
     print("Setup pins")
     GPIO.setup(pin, GPIO.OUT)
@@ -65,6 +94,17 @@ def to0grados():
         p.ChangeDutyCycle(duty_cycle)
         time.sleep(0.5)
 
+def compuertaAbierta():
+    for angle in range(60,-1,-60):
+        duty_cycle = 2.5 +(angle/18.0)
+        c.ChangeDutyCycle(duty_cycle)#Cuando esta abierto
+        time.sleep(0.5)
+
+def compuertaCerrada():
+    for angleY in range(0,60,60):
+        duty_cycle = 2.5 + (angleY/18.0)
+        c.ChangeDutyCycle(duty_cycle)
+        time.sleep(0.5)
 
 def steps(nb):
     StepCounter = 0
@@ -119,15 +159,175 @@ def posicionExpulsion(grados):
         steps(grados_a_pasos(grados))
 
 
+def deteccionMosquito():
+    while True:
+        # Lee el valor del pin GPIO
+        value = GPIO.input(pin_sensor)
+        estado = 0 #estados 0 aun no se detecto el mosquito, 1 se a detectado
+        if value == GPIO.HIGH:
+            print('Mosquito detectado: Se espera a que pase todo para cerrar la compuerta')
+            estado = 1
+            print(value)
+            return False
+            # Realiza acciones específicas para objetos blancos
+        else:
+            print('No hay mosquitos')
+            if estado == 1:
+                print('El mosquito a ingresado, proceder a cerrar la compuerta')
+                estado = 0
+                return True
+            print(value)
+            return False
+            # Realiza acciones específicas para objetos negros
+
+        # Espera un tiempo antes de la siguiente lectura
+        time.sleep(0.01)
+
+
+def es_dispositivo_usb(dispositivo):
+    # Verificar si el directorio del dispositivo contiene "usb"
+    return "usb" in dispositivo.device_path
+
+
+def obtener_dispositivos_usb():
+    context = pyudev.Context()
+    dispositivos_usb = []
+
+    for dispositivo in context.list_devices(subsystem='sound'):
+        # print("Atributos disponibles para el dispositivo:")
+        for atributo in dir(dispositivo.attributes):
+            if not atributo.startswith('_'):
+                valor = getattr(dispositivo.attributes, atributo)
+                # print(f"{atributo}: {valor}")
+
+        devtype = dispositivo.attributes.get('DEVTYPE')
+        id_bus = dispositivo.attributes.get('ID_BUS')
+
+        # print(f"devtype: {devtype}")
+        # print(f"id_bus: {id_bus}")
+
+        # Comprobamos si el dispositivo está en el bus USB
+        if es_dispositivo_usb(dispositivo):
+            dispositivos_usb.append(dispositivo.device_node)
+            print("Este es un dispositivo USB de audio.")
+        else:
+            print("Este no es un dispositivo USB de audio.")
+
+        print("\n---\n")
+
+    return dispositivos_usb
+
+
+def configurar_mic():
+    dispositivos_usb = obtener_dispositivos_usb()
+    if not dispositivos_usb:
+        # print("No se encontraron dispositivos USB.")
+        return None
+    # print("Dispositivos USB encontrados:")
+    # for i, dispositivo in enumerate(dispositivos_usb, 1):
+    # print(f'{i}.{dispositivo}')
+
+    seleccion = int(2)
+
+    if 1 <= seleccion <= len(dispositivos_usb):
+
+        # Configurar el micrófono seleccionado con 1 canal (mono)
+        mic_configurado = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL,
+                                        cardindex=2)  # Reemplaza 2 con tu cardindex
+        mic_configurado.setchannels(1)  # Establecer el número de canales a 1 (mono)
+        mic_configurado.setrate(frecuencia_muestreo)  # Establecer la frecuencia de muestreo a 44.1 kHz
+        mic_configurado.setformat(alsaaudio.PCM_FORMAT_S16_LE)  # Establecer el formato de audio a 16 bits little-endian
+
+        # Intenta con un tamaño de periodo más pequeño
+        mic_configurado.setperiodsize(longitud_senal)  # Establecer el tamaño del periodo
+
+        # Capturar datos
+        return mic_configurado
+    else:
+        print("Seleccion no valida")
+        return None
+def detectar_frecuencia_usb(capturador):
+    # Aplicar la Transformada Rapida de Fourier (FFT)
+
+    umbral_db = -50
+    plt.show(block=False)
+    frecuencias = np.fft.fftfreq(longitud_senal, 1 / frecuencia_muestreo)
+    amplitudes_iniciales = np.zeros(longitud_senal)
+    line, = ax.plot(frecuencias, amplitudes_iniciales)
+    try:
+        while True:
+
+            # Leer datos del microfono USB
+            longitud_buffer = len(capturador.read()[1])
+            longitud_buffer = longitud_buffer - (longitud_buffer % 2)
+
+            # Procesar los datos capturados
+            datos = np.frombuffer(capturador.read()[1][:longitud_buffer], dtype=np.int16)
+            # Realizar la FFT
+            # Calcular nivel de decibelios RMS
+            if datos is not None:
+                rms_level_db = 20 * np.log10(np.sqrt(np.mean(datos ** 2)))
+                # Calcular las frecuencias correspondientes
+                fft_resultado = np.fft.fft(datos)
+
+                # Calcular las frecuencias correspondientes
+                frecuencias = np.fft.fftfreq(longitud_senal, 1 / frecuencia_muestreo)
+
+                # Encontrar el indice de la frecuencia dominante
+                indice_frecuencia_dominante = np.argmax(np.abs(fft_resultado))
+
+                # Obtener la frecuencia dominante en Hz
+                # Actualizar la línea en el gráfico
+                line.set_xdata(np.abs(frecuencias))
+                line.set_ydata(fft_resultado)
+                # print(np.abs(fft_resultado))
+                plt.draw()
+                # Actualizar el gráfico
+                plt.pause(0.001)  # Añadi un pequeño retraso para permitir la actualización de la interfaz gráfica
+                frecuencia_dominante = frecuencias[indice_frecuencia_dominante]
+                if frecuencia_dominante != 0 and frecuencia_dominante >= 300 and rms_level_db > umbral_db:
+                    print(f'Decibelios:{rms_level_db}')
+                    print(f'Frecuencia: {frecuencia_dominante} Hz')
+                    time.sleep(0.01)
+                    return frecuencia_dominante
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Cerrar el micrófono al finalizar
+        #mic_configurado.close()
+        #plt.ioff()  # Desactivar el modo interactivo de Matplotlib
+        #plt.show(block=True)  # Esperar a que se cierre la ventana antes de finalizar
+
 if __name__ == '__main__':
     hasRun = False
     # GPIO.output(succionFan, GPIO.HIGH)
     to90grados()
     # GPIO.output(empujeFan, GPIO.LOW)
     time.sleep(5)
+    mic_configurado = configurar_mic()
+    #Se configura las etapa
+    #etapa = 0 no se detecta aun mosquito
+    #etapa = 1 se detecto el mosquito y a pasado al seleccionador para cerrar la compuerta
+    #etapa = 2 el mosquito paso todo para cerrar la compuerta y proceder a la clasificacion
     while not hasRun:
-        # steps(grados_a_pasos(siguiente*compuerta))# parcourt un tour dans le sens horaire
+        etapa = 0
+        if etapa == 0 :
+            compuertaAbierta() #Se mantiene abierto siempre que no haya mosquitos dentro del seleccionador
 
+        if deteccionMosquito()==True:
+            etapa = 1
+            compuertaCerrada()
+        if deteccionMosquito()==False and etapa == 1:
+            etapa = 2
+        if etapa==2:
+            print('Se procede a la clasificacion del mosquito')
+            # Configurar el microfono fuera de la funcion
+            # Ejecutar el detector de frecuencia con la configuracion del microfono
+            frecuencia = detectar_frecuencia_usb(mic_configurado)
+            if frecuencia >= 500 and frecuencia <=630:
+                print('se a detectado un mosquito')
+
+        # steps(grados_a_pasos(siguiente*compuerta))# parcourt un tour dans le sens horaire
         posicionExpulsion(siguiente * compuerta)
         # GPIO.output(succionFan, GPIO.LOW)
         # GPIO.output(empujeFan, GPIO.HIGH)
--- SensorInflarrojo.py
+++ SensorInflarrojo.py
@@ -10,18 +10,24 @@ pin_sensor = 5
 # Configura el pin como entrada
 GPIO.setup(pin_sensor, GPIO.IN)
 
-while True:
-    # Lee el valor del pin GPIO
-    value = GPIO.input(pin_sensor)
-    
-    if value == GPIO.HIGH:
-        print('Objeto blanco detectado')
-        print(value)
-        # Realiza acciones específicas para objetos blancos
-    else:
-        print('Objeto negro detectado')
-        print(value)
-        # Realiza acciones específicas para objetos negros
-    
-    # Espera un tiempo antes de la siguiente lectura
-    time.sleep(1)
+def deteccionMosquito():
+    while True:
+        # Lee el valor del pin GPIO
+        value = GPIO.input(pin_sensor)
+        estado = 0 #estados 0 aun no se detecto el mosquito, 1 se a detectado
+        if value == GPIO.HIGH:
+            print('Mosquito detectado: Se espera a que pase todo para cerrar la compuerta')
+            estado = 1
+            print(value)
+            # Realiza acciones específicas para objetos blancos
+        else:
+            print('No hay mosquitos')
+            if estado == 1:
+                print('El mosquito a ingresado, proceder a cerrar la compuerta')
+                estado = 0
+                return True
+            print(value)
+            # Realiza acciones específicas para objetos negros
+
+        # Espera un tiempo antes de la siguiente lectura
+        time.sleep(0.01)
