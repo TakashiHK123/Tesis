b7698eee7b4589b1d089d1b7ccc738b1d9c0c371
TakashiHK123 <taka.honge@gmail.com>
2023-12-25 18:18:07 -0300

    Ajustes

--- ClasificadorMosquito.py
+++ ClasificadorMosquito.py
@@ -161,33 +161,26 @@ def posicionExpulsion(grados):
         steps(grados_a_pasos(grados))
 
 
-def deteccionMosquito():
-    # Lee el valor del pin GPIO
-    value = GPIO.input(pin_sensor)
-    estado = 0 #estados 0 aun no se detecto el mosquito, 1 se a detectado
-    detected = False
-    while not detected:
+def deteccionMosquito(detected):
+    estado = 0  # estados 0 aun no se detecto el mosquito, 1 se a detectado
+    while True:
+        # Lee el valor del pin GPIO
+        value = GPIO.input(pin_sensor)
 
         if value == GPIO.HIGH:
             print('Mosquito detectado: Se espera a que pase todo para cerrar la compuerta')
             estado = 1
-            print(value)
-            return detected
+            print(f'Estado:{estado}')
             # Realiza acciones específicas para objetos blancos
         else:
-            print('No hay mosquitos')
+            print(f'Estado:{estado}')
             if estado == 1:
                 print('El mosquito a ingresado, proceder a cerrar la compuerta')
                 estado = 0
-                detected = True
-                return detected
+                break
             print(value)
-            return detected
+
             # Realiza acciones específicas para objetos negros
-
-        # Espera un tiempo antes de la siguiente lectura
-        time.sleep(0.01)
-
 
 def es_dispositivo_usb(dispositivo):
     # Verificar si el directorio del dispositivo contiene "usb"
