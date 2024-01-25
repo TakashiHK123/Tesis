9e631ada0003a0832bfcfd108cd5b5434b65ef30
TakashiHK123 <taka.honge@gmail.com>
2024-01-25 07:36:10 -0300

    ajustes

--- WebCam.py
+++ WebCam.py
@@ -2,10 +2,16 @@ import os
 import cv2
 
 def capturar_foto(nombre_archivo):
+    cap = cv2.VideoCapture(0)
+
+    # Verificar si la cámara está disponible
+    if not cap.isOpened():
+        print("Error: No se puede acceder a la cámara. ¿Está conectada correctamente?")
+        return
+
     directorio_actual = os.path.dirname(os.path.abspath(__file__))
     ruta_imagen = os.path.join(directorio_actual, nombre_archivo)
 
-    cap = cv2.VideoCapture(0)
     ret, frame = cap.read()
     cv2.imwrite(ruta_imagen, frame)
     cap.release()
