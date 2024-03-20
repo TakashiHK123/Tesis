666bf9043b3c2a074514c01e017e06a69b74416d
TakashiH <takahk888@gmail.com>
2024-03-19 21:23:33 -0300

    se ajusta la libreria a utilizar

--- PruebaMicrofono/100HzA2000Hz.py
+++ PruebaMicrofono/100HzA2000Hz.py
@@ -1,48 +1,43 @@
-import pyaudio
+import alsaaudio
 import numpy as np
 import matplotlib.pyplot as plt
 import wave
 
 class SoundDetector:
     def __init__(self):
-        self.FORMAT = pyaudio.paInt16
         self.CHANNELS = 1
         self.RATE = 44100
         self.CHUNK = 512
         self.RECORD_SECONDS = 5
-        self.p = pyaudio.PyAudio()
 
     def record_and_analyze(self, filename, save_plot_filename=None, input_device_index=None, repeat=False):
         while True:
+            # Abre el dispositivo de audio
             if input_device_index is None:
                 input_device_index = self.get_default_input_device_index()
 
-            stream = self.p.open(format=self.FORMAT,
-                                 channels=self.CHANNELS,
-                                 rate=self.RATE,
-                                 input=True,
-                                 frames_per_buffer=self.CHUNK,
-                                 input_device_index=input_device_index)
+            with alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL, input_device=input_device_index) as stream:
+                stream.setchannels(self.CHANNELS)
+                stream.setrate(self.RATE)
+                stream.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+                stream.setperiodsize(self.CHUNK)
 
-            print("Grabando...")
+                print("Grabando...")
 
-            frames = []
-            data = []
+                frames = []
+                data = []
 
-            for i in range(0, int(self.RATE / self.CHUNK * self.RECORD_SECONDS)):
-                audio_data = stream.read(self.CHUNK)
-                frames.append(audio_data)
-                data.extend(np.frombuffer(audio_data, dtype=np.int16))
+                for i in range(0, int(self.RATE / self.CHUNK * self.RECORD_SECONDS)):
+                    length, audio_data = stream.read()
+                    frames.append(audio_data)
+                    data.extend(np.frombuffer(audio_data, dtype=np.int16))
 
-            print("Fin de la grabación.")
-
-            stream.stop_stream()
-            stream.close()
+                print("Fin de la grabación.")
 
             # Guardar los datos de audio en un archivo WAV
             wf = wave.open(filename, 'wb')
             wf.setnchannels(self.CHANNELS)
-            wf.setsampwidth(self.p.get_sample_size(self.FORMAT))
+            wf.setsampwidth(2)  # 2 bytes para formato PCM_FORMAT_S16_LE
             wf.setframerate(self.RATE)
             wf.writeframes(b''.join(frames))
             wf.close()
@@ -54,7 +49,7 @@ class SoundDetector:
             fft_freq = np.fft.fftfreq(len(data), 1 / self.RATE)
             magnitude_spectrum = np.abs(fft_data)
 
-            # Identificar frecuencias que superan la magnitud de 1 de manera eficiente
+            # Identificar frecuencias que superan la magnitud de 0.2 dentro del rango de frecuencia especificado
             high_magnitude_indices = np.where((magnitude_spectrum > 0.2) & (fft_freq >= 100) & (fft_freq <= 2000))[0]
             high_magnitude_freq = fft_freq[high_magnitude_indices]
 
@@ -66,7 +61,7 @@ class SoundDetector:
             plt.title('Espectro de Magnitud')
             plt.grid(True)
 
-            # Resaltar las frecuencias que superan la magnitud de 1 dentro del rango de frecuencia especificado
+            # Resaltar las frecuencias que superan la magnitud de 0.2 dentro del rango de frecuencia especificado
             plt.plot(high_magnitude_freq, magnitude_spectrum[high_magnitude_indices], 'ro', markersize=5)
 
             plt.xlim(0, self.RATE / 15)  # Limitar la visualización a frecuencias positivas
@@ -78,14 +73,15 @@ class SoundDetector:
 
             plt.show()
 
-            # Imprimir las frecuencias que superan la magnitud de 1 dentro del rango de frecuencia especificado
-            print("Frecuencias que superan la magnitud de 1 dentro del rango de 100 Hz a 2000 Hz:", high_magnitude_freq)
+            # Imprimir las frecuencias que superan la magnitud de 0.2 dentro del rango de frecuencia especificado
+            print("Frecuencias que superan la magnitud de 0.2 dentro del rango de 100 Hz a 2000 Hz:", high_magnitude_freq)
 
             if not repeat:
                 break
 
     def get_default_input_device_index(self):
-        return self.p.get_default_input_device_info()["index"]
+        # Devuelve el primer índice de dispositivo
+        return 0
 
 # Uso de la clase SoundDetector
 detector = SoundDetector()
